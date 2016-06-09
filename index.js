6849d13cd815c4acc23e182b8e9715797100f191
Michael Wittig <post@michaelwittig.info>
2016-06-09 16:30:37 +0200

    implemeted auth

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
 "use strict";
 
-exports.handler = function(event, context) {
+var AWS = require("aws-sdk");
+var dynamodb = new AWS.DynamoDB();
+
+function generatePolicy(principalId, effect, resource) {
+  return {
+    "principalId": principalId,
+    "policyDocument": {
+      "Version": "2012-10-17",
+      "Statement": [{
+        "Action": "execute-api:Invoke",
+        "Effect": effect,
+        "Resource": resource
+      }]
+    }
+  };
+}
+
+exports.handler = function(event, context, cb) {
   console.log(JSON.stringify(event));
-  context.succeed({});
+  var token = event.authorizationToken;
+  dynamodb.getItem({
+    "Key": {
+      "token": {
+        "S": token
+      }
+    },
+    "TableName": "auth-token"
+  }, function(err, data) {
+    if (err) {
+      cb(err);
+    } else {
+      if (data.Item === undefined) {
+        cb(null, generatePolicy('user', 'Deny', event.methodArn));
+      } else {
+        cb(null, generatePolicy('user', 'Allow', event.methodArn));
+      }
+    }
+  });
 };
